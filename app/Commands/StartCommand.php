a82e0b3359ed00245d7b9e4aacf11339c3742e35
hossein <h.faghihi15@gmail.com>
2017-02-25 15:04:44 +0330

    testing changes in bot

--- app/Commands/StartCommand.php
+++ app/Commands/StartCommand.php
@@ -15,7 +15,7 @@ class StartCommand extends Command
     /**
      * @var string Command Description
      */
-    protected $description = "Start Command to get you started";
+    protected $description = "برای شروع";
 
     /**
      * @inheritdoc
