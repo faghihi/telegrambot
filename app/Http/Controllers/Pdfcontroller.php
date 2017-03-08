2612bdeeab15e8a65cf09e229c681dfd85a079f3
hossein <h.faghihi15@gmail.com>
2017-03-08 20:03:14 +0330

    pdf pdf

--- app/Http/Controllers/Pdfcontroller.php
+++ app/Http/Controllers/Pdfcontroller.php
@@ -26,9 +26,7 @@ class Pdfcontroller extends Controller
         PDF::AddPage();
         PDF::writeHTML(view('pdf', ['salam'=>'اینم'])->render());
 //        PDF::output('salam.pdf');
-        $filename = '/uploads'.'test'.'.pdf';
-        PDF::output($filename, 'I');
+        $filename = public_path().'/uploads/salam.pdf';
         PDF::output($filename, 'F');
-        return 1;
     }
 }
