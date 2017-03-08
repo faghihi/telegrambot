a1afaae2f728856bf6bc54ed8ec5d6114639af45
hossein <h.faghihi15@gmail.com>
2017-03-08 17:42:00 +0330

    pdf pdf

--- app/Http/Controllers/Pdfcontroller.php
+++ app/Http/Controllers/Pdfcontroller.php
@@ -10,7 +10,14 @@ class Pdfcontroller extends Controller
     public function index()
     {
         $html = '<h1>سلام</h1>';
+        $lg = Array();
+        $lg['a_meta_charset'] = 'UTF-8';
+        $lg['a_meta_dir'] = 'rtl';
+        $lg['a_meta_language'] = 'fa';
+        $lg['w_page'] = 'page';
 
+// set some language-dependent strings (optional)
+        PDF::setLanguageArray($lg);
         PDF::SetTitle('Hello World');
         PDF::AddPage();
         PDF::writeHTML($html, true, false, true, false, '');
