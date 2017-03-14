383270d02ea8a54000db0c7b12bf4fb4ee8fba31
hossein <h.faghihi15@gmail.com>
2017-03-14 13:58:14 +0330

    pdf controllertest

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -4,14 +4,17 @@
     <title>Bootstrap Example</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .test{
+            font-size: 40px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 
 <div class="container">
-    <div class="alert">
+    <div class="test">
         salam
     </div>
     <h2>{{$salam}}</h2>
