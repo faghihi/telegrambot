77bba1037831a1c189c4ea7b563d2856964c9e3a
hossein <h.faghihi15@gmail.com>
2017-04-12 12:02:29 +0430

    changes test

--- app/Http/Controllers/Pdfcontroller.php
+++ app/Http/Controllers/Pdfcontroller.php
@@ -19,20 +19,29 @@ class Pdfcontroller extends Controller
         $lg['a_meta_dir'] = 'rtl';
         $lg['a_meta_language'] = 'fa';
         $lg['w_page'] = 'page';
-        $fontname = TCPDF_FONTS::addTTFfont(public_path().'/fonts/bnazanin.ttf', 'TrueTypeUnicode', '', 96);
-//        dd($fontname);
+        $fontname = TCPDF_FONTS::addTTFfont(public_path().'/fonts/titrbold.ttf', 'TrueTypeUnicode', '', 120);
         PDF::SetFont($fontname, '', 12, '', false);
 
 // set some language-dependent strings (optional)
         PDF::setLanguageArray($lg);
         PDF::SetTitle('Hello World');
         PDF::AddPage();
-        PDF::writeHTML(view('pdf', ['salam'=>'سلام'])->render());
+        PDF::writeHTML(view('pdf', ['salam'=>'تیتر'])->render());
+        $fontname = TCPDF_FONTS::addTTFfont(public_path().'/fonts/nazanin.ttf', 'TrueTypeUnicode', '', 120);
+        PDF::SetFont($fontname, '', 12, '', false);
+        PDF::writeHTML(view('pdf', ['salam'=>'نازنین'])->render());
+        $fontname = TCPDF_FONTS::addTTFfont(public_path().'/fonts/mitra.ttf', 'TrueTypeUnicode', '', 120);
+        PDF::SetFont($fontname, '', 12, '', false);
+        PDF::writeHTML(view('pdf', ['salam'=>'میترا'])->render());
         $pages=PDF::getNumPages	();
-        dd($pages);
+        $fontname = TCPDF_FONTS::addTTFfont(public_path().'/fonts/roya.ttf', 'TrueTypeUnicode', '', 120);
+        PDF::SetFont($fontname, '', 12, '', false);
+        PDF::writeHTML(view('pdf', ['salam'=>'رویا'])->render());
+
 //        PDF::writeHTML(view('pdf', ['salam'=>$pages])->render());
-//
-////        $filename = public_path().'/uploads/salam.pdf';
-//        PDF::output('resume.pdf', 'I');
+
+//        PDF::output('salam.pdf');
+//        $filename = public_path().'/uploads/salam.pdf';
+        PDF::output('resume.pdf', 'I');
     }
 }
